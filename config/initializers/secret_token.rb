85593af814ee0c6b571a2b9fa5aa09ab247366d2
Skaradams <skaradams@ubuntu.(none)>
2013-10-23 21:34:09 +0200

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tigerlily::Application.config.secret_token = '3801932c87c0683451f8a93b2b1134aee02dd9c169a04ae60fdd80b327bfda12039bd5edf32b4b55cff67712ba288da35235386bb9e459826dfbcd734fd60da0'
